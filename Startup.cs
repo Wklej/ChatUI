32e0071d71fc9190693156f8cd03d0c993c4fb2f
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-22 00:10:34 +0200

    +ConnectionString to database

--- Startup.cs
+++ Startup.cs
@@ -1,27 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ChatUI.DB;
+using ChatUI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace ChatUI
 {
     public class Startup
     {
+        private IConfiguration _config;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration conifg)
+        {
+            _config = conifg;
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddMvc(options => options.EnableEndpointRouting = false);   
-            services.AddDbContext<AppDbContext>();
+            var connectionString = _config.GetConnectionString("DefaultConnection");
 
+            services.AddMvc(options => options.EnableEndpointRouting = false);   
+            
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(connectionString)
+            );
+            
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
 
         }
 
