bcbe89332998b56879502153ecb385ffba84d7b0
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-21 23:53:23 +0200

    Fixes dependencies import

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatUI.DB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -18,7 +19,9 @@ namespace ChatUI
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddMvc(options => options.EnableEndpointRouting = false);         
+            services.AddMvc(options => options.EnableEndpointRouting = false);   
+            services.AddDbContext<AppDbContext>();
+
 
         }
 
