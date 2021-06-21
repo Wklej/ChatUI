64b7c2fc88c63f5ddf7dbbe6b4da4fa9c6c8f673
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-21 23:35:26 +0200

    +DB

---
+++ DB/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ChatUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatUI.DB
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        
+
+    }
+}
