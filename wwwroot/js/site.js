8c491084fcc3f7024f71f5fffce20d5ca0ae12aa
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-25 18:37:15 +0200

    +scritps +closing modal

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿var createRoomButton = document.getElementById('create-room-button')
+var createRoomModal = document.getElementById('create-room-modal')
+
+createRoomButton.addEventListener('click', function () {
+    createRoomModal.classList.add('active')
+
+})
+
+var closeModal = function() {
+    createRoomModal.classList.remove('active')
+}
