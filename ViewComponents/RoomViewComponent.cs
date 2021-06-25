7e8e1b33eddd67f9101eb058cc2d6d5c353b1948
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-25 19:14:26 +0200

    +Adding rooms to UI and DB

---
+++ ViewComponents/RoomViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using ChatUI.DB;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatUI.ViewComponents
+{
+    public class RoomViewComponent : ViewComponent
+    {
+        AppDbContext _context;
+
+        public RoomViewComponent(AppDbContext context) => _context = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var chats = _context.Chats.ToList();
+
+            return View(chats);
+        }
+    }
+}
