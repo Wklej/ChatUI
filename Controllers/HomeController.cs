3a80ddf62319cd2a19632e904aff339940b9c2b2
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-26 12:19:51 +0200

    +Authorization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ChatUI.DB;
 using ChatUI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace ChatUI.Controllers
 {
+    [Authorize]
     public class HomeController: Controller
     {
         private AppDbContext _context;
