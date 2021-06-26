806547affa9bf1b2e75404ae977b8e7f68dcaa9b
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-26 12:47:15 +0200

    +JoinRoom

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ChatUI.Controllers
@@ -21,13 +22,13 @@ namespace ChatUI.Controllers
 
         [HttpGet("{id}")]
         public IActionResult Chat(int id)
-        {           
+        {
             var chat = _context.Chats
                 .Include(x => x.Messages)
                 .FirstOrDefault(x => x.Id == id);
 
             return View(chat);
-        }
+        }        
 
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int chatId, string message)
@@ -51,11 +52,36 @@ namespace ChatUI.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateRoom(string name)
         {
-            _context.Chats.Add(new Chat
+            var chat = new Chat
             {
                 Name = name,
                 Type = ChatType.ROOM
-            }); ;
+            };
+
+            chat.Users.Add(new ChatUser
+            {
+                UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value,
+                Role = UserRole.Admin
+            });
+
+            _context.Chats.Add(chat);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> JoinRoom(int id)
+        {
+            var chatUser = new ChatUser
+            {
+                ChatId = id,
+                UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value,
+                Role = UserRole.Member
+            };
+
+            _context.ChatUsers.Add(chatUser);
 
             await _context.SaveChangesAsync();
 
