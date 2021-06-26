db9a8d8f9a6db164530c37c7cdc0c75900b524bc
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-26 12:40:45 +0200

    update

--- Models/Chat.cs
+++ Models/Chat.cs
@@ -10,13 +10,13 @@ namespace ChatUI.Models
         public Chat()
         {
             Messages = new List<Message>();
-            Users = new List<User>();
+            Users = new List<ChatUser>();
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Message> Messages { get; set; }
-        public ICollection<User> Users { get; set; }
+        public ICollection<ChatUser> Users { get; set; }
         public ChatType Type { get; set; }            
     }
 
--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace ChatUI.Models
 {
     public class User : IdentityUser
     {
-
+        public ICollection<ChatUser> Chats { get; set; }
     }
 
 }
